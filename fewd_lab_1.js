b11fc948bd4e3aa28d7e22b8dae38f0fb48e90fb
Michele Sousa <21959@student.dorset-college.ie>
2020-12-16 09:56:02 +0000

    ‘commit’

---
+++ fewd_lab_1.js
@@ -0,0 +1,22 @@
+var FirstName = 'Michele';
+var favouriteFood = 'Beef';
+var favouriteColor = "blue";
+var city = 'Dublin';
+
+//Display the value of this variables:
+console.log(FirstName);
+console.log(favouriteFood);
+console.log(favouriteFood + ' ' + city);
+
+var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.';
+var endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see!';
+var one = 'Red';
+var two = 'orange';
+var three = 'yellow';
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var seven = 'violet';
+
+console.log(phrase + ' ' + one + ', ' + two + ', ' + three + ', ' + four +', '+ five + ', ' + six + ', ' + seven + ' ' + endingPhrase);
+
